4991610c436c0946fa8e639ee71a5f7b9edb01a5
craig.hinrichs@gmail.com <craig.hinrichs@gmail.com>
2025-06-07 13:40:21 -0700

    - Added groupDropDown Obsidian File

---
+++ Rock.JavaScript.Obsidian/Framework/ViewModels/Utility/listGroupsOptionsBag.ts
@@ -0,0 +1,10 @@
+export type ListGroupsOptionsBag = {
+    /** Gets or sets the category for this item. */
+    guid?: string | null;
+
+    /** Gets or sets disabled for this item. */
+    name?: string | null;
+
+    /** Gets or sets the text. */
+    isactive?: boolean | null;
+};
